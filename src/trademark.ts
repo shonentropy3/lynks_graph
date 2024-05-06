e8b52d4688773b711884b4b8440b5697e7d57e04
MickWang <1244134672@qq.com>
2024-05-06 20:37:28 +0800

    update for trademark balance mapping

--- src/trademark.ts
+++ src/trademark.ts
@@ -4,8 +4,10 @@ import {
 } from "../generated/trademark/trademark"
 import {
   TransferBatch,
-  TransferSingle
+  TransferSingle,
+  TrademarkBalance
 } from "../generated/schema"
+import { Bytes, crypto, BigInt } from "@graphprotocol/graph-ts";
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
   let entity = new TransferBatch(
@@ -22,6 +24,32 @@ export function handleTransferBatch(event: TransferBatchEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+
+  for(let i = 0; i < event.params.ids.length; i++) {
+    let id = event.params.ids[i]
+    let value = event.params.values[i]
+    let trademarkBalanceFrom = TrademarkBalance.load(event.params.from.concatI32(id.toI32()))
+    if(trademarkBalanceFrom === null) {
+      trademarkBalanceFrom = new TrademarkBalance(event.params.from.concatI32(id.toI32()))
+      trademarkBalanceFrom.trademark_id = id
+      trademarkBalanceFrom.address = event.params.from
+      trademarkBalanceFrom.balance = BigInt.fromI32(0);
+    }
+    trademarkBalanceFrom.balance = trademarkBalanceFrom.balance.minus(value)
+    trademarkBalanceFrom.save()
+  
+
+    let trademarkBalanceTo = TrademarkBalance.load(event.params.to.concatI32(id.toI32()))
+    if(trademarkBalanceTo === null) {
+      trademarkBalanceTo = new TrademarkBalance(event.params.to.concatI32(id.toI32()))
+      trademarkBalanceTo.trademark_id = id
+      trademarkBalanceTo.address = event.params.to
+      trademarkBalanceTo.balance = BigInt.fromI32(0);
+    }
+    trademarkBalanceTo.balance = trademarkBalanceTo.balance.plus(value)
+    trademarkBalanceTo.save()
+}
 }
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
@@ -39,4 +67,24 @@ export function handleTransferSingle(event: TransferSingleEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let trademarkBalanceFrom = TrademarkBalance.load(event.params.from.concatI32(event.params.id.toI32()))
+  if(trademarkBalanceFrom === null) {
+    trademarkBalanceFrom = new TrademarkBalance(event.params.from.concatI32(event.params.id.toI32()))
+    trademarkBalanceFrom.trademark_id = event.params.id
+    trademarkBalanceFrom.address = event.params.from
+    trademarkBalanceFrom.balance = new BigInt(0)
+  }
+  trademarkBalanceFrom.balance = trademarkBalanceFrom.balance.minus(event.params.value)
+  trademarkBalanceFrom.save()
+
+  let trademarkBalanceTo = TrademarkBalance.load(event.params.to.concatI32(event.params.id.toI32()))
+  if(trademarkBalanceTo === null) {
+    trademarkBalanceTo = new TrademarkBalance(event.params.to.concatI32(event.params.id.toI32()))
+    trademarkBalanceTo.trademark_id = event.params.id
+    trademarkBalanceTo.address = event.params.to
+    trademarkBalanceTo.balance = new BigInt(0)
+  }
+  trademarkBalanceTo.balance = trademarkBalanceTo.balance.plus(event.params.value)
+  trademarkBalanceTo.save()
 }
