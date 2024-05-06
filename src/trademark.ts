7cf7ce484c4e62d6919376bf5b16989912a35250
shonentropy3 <qwerdfkkt@163.com>
2024-05-06 17:10:29 +0800

    graph

---
+++ src/trademark.ts
@@ -0,0 +1,42 @@
+import {
+  TransferBatch as TransferBatchEvent,
+  TransferSingle as TransferSingleEvent
+} from "../generated/trademark/trademark"
+import {
+  TransferBatch,
+  TransferSingle
+} from "../generated/schema"
+
+export function handleTransferBatch(event: TransferBatchEvent): void {
+  let entity = new TransferBatch(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.operator = event.params.operator
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.ids = event.params.ids
+  entity.values = event.params.values
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransferSingle(event: TransferSingleEvent): void {
+  let entity = new TransferSingle(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.operator = event.params.operator
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.trademark_id = event.params.id
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
