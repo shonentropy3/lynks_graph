0ee9b100665f8c856c83927d67863c8899898053
MickWang <1244134672@qq.com>
2024-05-06 18:36:10 +0800

    add balance mapping

--- src/lynks.ts
+++ src/lynks.ts
@@ -2,9 +2,10 @@ import {
   Transfer as TransferEvent
 } from "../generated/lynks/lynks"
 import {
-  Transfer
+  Transfer,
+  LynksBalance
 } from "../generated/schema"
-
+import { BigInt } from '@graphprotocol/graph-ts'
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
@@ -18,4 +19,23 @@ export function handleTransfer(event: TransferEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let fromBalance = LynksBalance.load(event.params.from)
+  if(fromBalance === null) {
+    fromBalance = new LynksBalance(event.params.from)
+    fromBalance.address = event.params.from;
+    fromBalance.balance = new BigInt(0);
+  }
+  fromBalance.balance  = fromBalance.balance.minus(new BigInt(1));
+  fromBalance.save();
+
+  let toBalance = LynksBalance.load(event.params.to)
+  if(toBalance === null) {
+    toBalance = new LynksBalance(event.params.to)
+    toBalance.address = event.params.to;
+    toBalance.balance = new BigInt(0);
+  }
+  toBalance.balance = toBalance.balance.plus(new BigInt(1));
+  toBalance.save();
+  
 }
