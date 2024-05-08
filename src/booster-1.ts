08b72a23e159392949fb5586ff7951d58444e39e
MickWang <1244134672@qq.com>
2024-05-08 14:29:18 +0800

    update booster mapping

--- src/booster-1.ts
+++ src/booster-1.ts
@@ -6,201 +6,82 @@ import {
   TransferBatch,
   TransferSingle,
   Boost1Balance,
-  Boost2Balance
-} from "../generated/schema"
+} from "../generated/schema";
 
 import { Bytes, BigInt, crypto } from "@graphprotocol/graph-ts";
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
-  let entity = new TransferBatch(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.ids = event.params.ids
-  entity.values = event.params.values
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let booster1Amount1 = loadBooster1Amount(event.params.to);
+  let booster1Amount2 = loadBooster1Amount(event.params.from);
+  for (let i = 0; i < event.params.ids.length; i++) {
+    let id = event.params.ids[i];
+    let value = event.params.values[i];
 
-  for(let i = 0; i < event.params.ids.length; i++) {
-    let id = event.params.ids[i]
-    let value = event.params.values[i]
-    let booster1Amount1 = loadBooster1Amount(event.params.to)
-  let booster2Amount1 = loadBooster2Amount(event.params.to)
-
-    if(id == BigInt.fromI32(3)) {
-      booster1Amount1.balance3 = booster1Amount1.balance3.plus(value)
+    if (id == BigInt.fromI32(3)) {
+      booster1Amount1.balance3 = booster1Amount1.balance3.plus(value);
+      booster1Amount2.balance3 = booster1Amount2.balance3.minus(value);
+    } else if (id == BigInt.fromI32(4)) {
+      booster1Amount1.balance4 = booster1Amount1.balance4.plus(value);
+      booster1Amount2.balance4 = booster1Amount2.balance4.minus(value);
+    } else if (id == BigInt.fromI32(100)) {
+      booster1Amount1.balance100 = booster1Amount1.balance100.plus(value);
+      booster1Amount2.balance100 = booster1Amount2.balance100.minus(value);
+    } else if (id == BigInt.fromI32(300)) {
+      booster1Amount1.balance300 = booster1Amount1.balance300.plus(value);
+      booster1Amount2.balance300 = booster1Amount2.balance300.minus(value);
+    } else if (id == BigInt.fromI32(500)) {
+      booster1Amount1.balance500 = booster1Amount1.balance500.plus(value);
+      booster1Amount2.balance500 = booster1Amount2.balance500.minus(value);
+    } else if (id == BigInt.fromI32(1000)) {
+      booster1Amount1.balance1000 = booster1Amount1.balance1000.plus(value);
+      booster1Amount2.balance1000 = booster1Amount2.balance1000.minus(value);
+    } else if (id == BigInt.fromI32(2000)) {
+      booster1Amount1.balance2000 = booster1Amount1.balance2000.plus(value);
+      booster1Amount2.balance2000 = booster1Amount2.balance2000.minus(value);
     }
-    if(id == BigInt.fromI32(4)) {
-      booster1Amount1.balance4 = booster1Amount1.balance4.plus(value)
-    }
-    if(id == BigInt.fromI32(50)) {
-      booster2Amount1.balance50 = booster2Amount1.balance50.plus(value)
-    }
-    if(id == BigInt.fromI32(100)) {
-      booster1Amount1.balance100 = booster1Amount1.balance100.plus(value)
-      booster2Amount1.balance100 = booster2Amount1.balance100.plus(value)
-    }
-    if(id == BigInt.fromI32(200)) {
-      booster2Amount1.balance200 = booster2Amount1.balance200.plus(value)
-    }
-    if(id == BigInt.fromI32(300)) {
-      booster1Amount1.balance300 = booster1Amount1.balance300.plus(value)
-    }
-    if(id == BigInt.fromI32(500)) {
-      booster1Amount1.balance500 = booster1Amount1.balance500.plus(value)
-      booster2Amount1.balance500 = booster2Amount1.balance500.plus(value)
-    }
-    if(id == BigInt.fromI32(1000)) {
-      booster1Amount1.balance1000 = booster1Amount1.balance1000.plus(value)
-      booster2Amount1.balance1000 = booster2Amount1.balance1000.plus(value)
-    }
-    if(id == BigInt.fromI32(2000)) {
-      booster1Amount1.balance2000 = booster1Amount1.balance2000.plus(value)
-    }
-    booster1Amount1.save()
-    booster2Amount1.save()
-
-    let booster1Amount2 = loadBooster1Amount(event.params.from)
-    let booster2Amount2 = loadBooster2Amount(event.params.from)
-
-    if(id == BigInt.fromI32(3)) {
-      booster1Amount2.balance3 = booster1Amount2.balance3.minus(value)
-    }
-    if(id == BigInt.fromI32(4)) {
-      booster1Amount2.balance4 = booster1Amount2.balance4.minus(value)
-    }
-    if(id == BigInt.fromI32(50)) {
-      booster2Amount2.balance50 = booster2Amount2.balance50.minus(value)
-    }
-    if(id == BigInt.fromI32(100)) {
-      booster1Amount2.balance100 = booster1Amount2.balance100.minus(value)
-      booster2Amount2.balance100 = booster2Amount2.balance100.minus(value)
-    }
-    if(id == BigInt.fromI32(200)) {
-      booster2Amount2.balance200 = booster2Amount2.balance200.minus(value)
-    }
-    if(id == BigInt.fromI32(300)) {
-      booster1Amount2.balance300 = booster1Amount2.balance300.minus(value)
-    }
-    if(id == BigInt.fromI32(500)) {
-      booster1Amount2.balance500 = booster1Amount2.balance500.minus(value)
-      booster2Amount2.balance500 = booster2Amount2.balance500.minus(value)
-    }
-    if(id == BigInt.fromI32(1000)) {
-      booster1Amount2.balance1000 = booster1Amount2.balance1000.plus(value)
-      booster2Amount2.balance1000 = booster2Amount2.balance1000.plus(value)
-    }
-    if(id == BigInt.fromI32(2000)) {
-      booster1Amount2.balance2000 = booster1Amount2.balance2000.plus(value)
-    }
-    booster1Amount2.save()
-    booster2Amount2.save()
   }
-
-  entity.save()
+  booster1Amount1.save();
+  booster1Amount2.save();
 }
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
-  let entity = new TransferSingle(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.trademark_id = event.params.id
-  entity.value = event.params.value
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let value = event.params.value;
+  let id = event.params.id;
 
-  entity.save()
+  let booster1Amount1 = loadBooster1Amount(event.params.to);
+  let booster1Amount2 = loadBooster1Amount(event.params.from);
 
-  let value = event.params.value
-  let id = event.params.id
-
-  let booster1Amount1 = loadBooster1Amount(event.params.to)
-  let booster2Amount1 = loadBooster2Amount(event.params.to)
-
-    if(id == BigInt.fromI32(3)) {
-      booster1Amount1.balance3 = booster1Amount1.balance3.plus(value)
-    }
-    if(id == BigInt.fromI32(4)) {
-      booster1Amount1.balance4 = booster1Amount1.balance4.plus(value)
-    }
-    if(id == BigInt.fromI32(50)) {
-      booster2Amount1.balance50 = booster2Amount1.balance50.plus(value)
-    }
-    if(id == BigInt.fromI32(100)) {
-      booster1Amount1.balance100 = booster1Amount1.balance100.plus(value)
-      booster2Amount1.balance100 = booster2Amount1.balance100.plus(value)
-    }
-    if(id == BigInt.fromI32(200)) {
-      booster2Amount1.balance200 = booster2Amount1.balance200.plus(value)
-    }
-    if(id == BigInt.fromI32(300)) {
-      booster1Amount1.balance300 = booster1Amount1.balance300.plus(value)
-    }
-    if(id == BigInt.fromI32(500)) {
-      booster1Amount1.balance500 = booster1Amount1.balance500.plus(value)
-      booster2Amount1.balance500 = booster2Amount1.balance500.plus(value)
-    }
-    if(id == BigInt.fromI32(1000)) {
-      booster1Amount1.balance1000 = booster1Amount1.balance1000.plus(value)
-      booster2Amount1.balance1000 = booster2Amount1.balance1000.plus(value)
-    }
-    if(id == BigInt.fromI32(2000)) {
-      booster1Amount1.balance2000 = booster1Amount1.balance2000.plus(value)
-    }
-    booster1Amount1.save()
-    booster2Amount1.save()
-
-    let booster1Amount2 = loadBooster1Amount(event.params.from)
-    let booster2Amount2 = loadBooster2Amount(event.params.from)
-
-    if(id == BigInt.fromI32(3)) {
-      booster1Amount2.balance3 = booster1Amount2.balance3.minus(value)
-    }
-    if(id == BigInt.fromI32(4)) {
-      booster1Amount2.balance4 = booster1Amount2.balance4.minus(value)
-    }
-    if(id == BigInt.fromI32(50)) {
-      booster2Amount2.balance50 = booster2Amount2.balance50.minus(value)
-    }
-    if(id == BigInt.fromI32(100)) {
-      booster1Amount2.balance100 = booster1Amount2.balance100.minus(value)
-      booster2Amount2.balance100 = booster2Amount2.balance100.minus(value)
-    }
-    if(id == BigInt.fromI32(200)) {
-      booster2Amount2.balance200 = booster2Amount2.balance200.minus(value)
-    }
-    if(id == BigInt.fromI32(300)) {
-      booster1Amount2.balance300 = booster1Amount2.balance300.minus(value)
-    }
-    if(id == BigInt.fromI32(500)) {
-      booster1Amount2.balance500 = booster1Amount2.balance500.minus(value)
-      booster2Amount2.balance500 = booster2Amount2.balance500.minus(value)
-    }
-    if(id == BigInt.fromI32(1000)) {
-      booster1Amount2.balance1000 = booster1Amount2.balance1000.plus(value)
-      booster2Amount2.balance1000 = booster2Amount2.balance1000.plus(value)
-    }
-    if(id == BigInt.fromI32(2000)) {
-      booster1Amount2.balance2000 = booster1Amount2.balance2000.plus(value)
-    }
-    booster1Amount2.save()
-    booster2Amount2.save()
+  if (id == BigInt.fromI32(3)) {
+    booster1Amount1.balance3 = booster1Amount1.balance3.plus(value);
+    booster1Amount2.balance3 = booster1Amount2.balance3.minus(value);
+  } else if (id == BigInt.fromI32(4)) {
+    booster1Amount1.balance4 = booster1Amount1.balance4.plus(value);
+    booster1Amount2.balance4 = booster1Amount2.balance4.minus(value);
+  } else if (id == BigInt.fromI32(100)) {
+    booster1Amount1.balance100 = booster1Amount1.balance100.plus(value);
+    booster1Amount2.balance100 = booster1Amount2.balance100.minus(value);
+  } else if (id == BigInt.fromI32(300)) {
+    booster1Amount1.balance300 = booster1Amount1.balance300.plus(value);
+    booster1Amount2.balance300 = booster1Amount2.balance300.minus(value);
+  } else if (id == BigInt.fromI32(500)) {
+    booster1Amount1.balance500 = booster1Amount1.balance500.plus(value);
+    booster1Amount2.balance500 = booster1Amount2.balance500.minus(value);
+  } else if (id == BigInt.fromI32(1000)) {
+    booster1Amount1.balance1000 = booster1Amount1.balance1000.plus(value);
+    booster1Amount2.balance1000 = booster1Amount2.balance1000.minus(value);
+  } else if (id == BigInt.fromI32(2000)) {
+    booster1Amount1.balance2000 = booster1Amount1.balance2000.plus(value);
+    booster1Amount2.balance2000 = booster1Amount2.balance2000.minus(value);
+  }
+  booster1Amount1.save();
+  booster1Amount2.save();
 }
 
-export function loadBooster1Amount(address: Bytes): Boost1Balance{
-  const id = Bytes.fromByteArray(
-    crypto.keccak256(address)
-  );
-  let boost1Balance = Boost1Balance.load(id)
+export function loadBooster1Amount(address: Bytes): Boost1Balance {
+  const id = Bytes.fromByteArray(crypto.keccak256(address));
+  let boost1Balance = Boost1Balance.load(id);
   if (!boost1Balance) {
     boost1Balance = new Boost1Balance(id);
     boost1Balance.address = address;
@@ -211,27 +92,7 @@ export function loadBooster1Amount(address: Bytes): Boost1Balance{
     boost1Balance.balance500 = BigInt.fromI32(0);
     boost1Balance.balance1000 = BigInt.fromI32(0);
     boost1Balance.balance2000 = BigInt.fromI32(0);
-    // trademarkAmount.save();
-    boost1Balance.save()
+    boost1Balance.save();
   }
   return boost1Balance;
-}
-
-export function loadBooster2Amount(address: Bytes): Boost2Balance{
-  const id = Bytes.fromByteArray(
-    crypto.keccak256(address)
-  );
-  let boost2Balance = Boost2Balance.load(id)
-  if (!boost2Balance) {
-    boost2Balance = new Boost2Balance(id);
-    boost2Balance.address = address;
-    boost2Balance.balance50 = BigInt.fromI32(0);
-    boost2Balance.balance100 = BigInt.fromI32(0);
-    boost2Balance.balance200 = BigInt.fromI32(0);
-    boost2Balance.balance500 = BigInt.fromI32(0);
-    boost2Balance.balance1000 = BigInt.fromI32(0);
-    // trademarkAmount.save();
-    boost2Balance.save()
-  }
-  return boost2Balance;
 }
---
+++ src/booster-2.ts
@@ -0,0 +1,86 @@
+import {
+  TransferBatch as TransferBatchEvent,
+  TransferSingle as TransferSingleEvent,
+} from "../generated/booster_1/booster_1";
+import {
+  TransferBatch,
+  TransferSingle,
+  Boost2Balance,
+} from "../generated/schema";
+import { Bytes, BigInt, crypto } from "@graphprotocol/graph-ts";
+
+export function handleTransferBatch(event: TransferBatchEvent): void {
+  let booster2AmountTo = loadBooster2Amount(event.params.to);
+  let booster2AmountFrom = loadBooster2Amount(event.params.from);
+
+  for (let i = 0; i < event.params.ids.length; i++) {
+    let id = event.params.ids[i];
+    let value = event.params.values[i];
+    if (id == BigInt.fromI32(50)) {
+      booster2AmountTo.balance50 = booster2AmountTo.balance50.plus(value);
+      booster2AmountFrom.balance50 = booster2AmountFrom.balance50.minus(value);
+    } else if (id == BigInt.fromI32(100)) {
+      booster2AmountTo.balance100 = booster2AmountTo.balance100.plus(value);
+      booster2AmountFrom.balance100 =
+        booster2AmountFrom.balance100.minus(value);
+    } else if (id == BigInt.fromI32(200)) {
+      booster2AmountTo.balance200 = booster2AmountTo.balance200.plus(value);
+      booster2AmountFrom.balance200 =
+        booster2AmountFrom.balance200.minus(value);
+    } else if (id == BigInt.fromI32(500)) {
+      booster2AmountTo.balance500 = booster2AmountTo.balance500.plus(value);
+      booster2AmountFrom.balance500 =
+        booster2AmountFrom.balance500.minus(value);
+    } else if (id == BigInt.fromI32(1000)) {
+      booster2AmountTo.balance1000 = booster2AmountTo.balance1000.plus(value);
+      booster2AmountFrom.balance1000 =
+        booster2AmountFrom.balance1000.minus(value);
+    }
+  }
+  booster2AmountFrom.save();
+  booster2AmountTo.save();
+}
+
+export function handleTransferSingle(event: TransferSingleEvent): void {
+  let booster2AmountTo = loadBooster2Amount(event.params.to);
+  let booster2AmountFrom = loadBooster2Amount(event.params.from);
+  let value = event.params.value;
+  let id = event.params.id;
+
+  if (id == BigInt.fromI32(50)) {
+    booster2AmountTo.balance50 = booster2AmountTo.balance50.plus(value);
+    booster2AmountFrom.balance50 = booster2AmountFrom.balance50.minus(value);
+  } else if (id == BigInt.fromI32(100)) {
+    booster2AmountTo.balance100 = booster2AmountTo.balance100.plus(value);
+    booster2AmountFrom.balance100 = booster2AmountFrom.balance100.minus(value);
+  } else if (id == BigInt.fromI32(200)) {
+    booster2AmountTo.balance200 = booster2AmountTo.balance200.plus(value);
+    booster2AmountFrom.balance200 = booster2AmountFrom.balance200.minus(value);
+  } else if (id == BigInt.fromI32(500)) {
+    booster2AmountTo.balance500 = booster2AmountTo.balance500.plus(value);
+    booster2AmountFrom.balance500 = booster2AmountFrom.balance500.minus(value);
+  } else if (id == BigInt.fromI32(1000)) {
+    booster2AmountTo.balance1000 = booster2AmountTo.balance1000.plus(value);
+    booster2AmountFrom.balance1000 =
+      booster2AmountFrom.balance1000.minus(value);
+  }
+  booster2AmountFrom.save();
+  booster2AmountTo.save();
+}
+
+export function loadBooster2Amount(address: Bytes): Boost2Balance {
+  const id = Bytes.fromByteArray(crypto.keccak256(address));
+  let boost2Balance = Boost2Balance.load(id);
+  if (!boost2Balance) {
+    boost2Balance = new Boost2Balance(id);
+    boost2Balance.address = address;
+    boost2Balance.balance50 = BigInt.fromI32(0);
+    boost2Balance.balance100 = BigInt.fromI32(0);
+    boost2Balance.balance200 = BigInt.fromI32(0);
+    boost2Balance.balance500 = BigInt.fromI32(0);
+    boost2Balance.balance1000 = BigInt.fromI32(0);
+    // trademarkAmount.save();
+    boost2Balance.save();
+  }
+  return boost2Balance;
+}
